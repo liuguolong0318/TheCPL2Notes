a6f80db6b84464d9ba12f73b64f584e2ea4b9068
liuguolong.steven <liuguolong.steven@bytedance.com>
2021-11-05 15:04:37 +0800

    Initial Commit

---
+++ TheCPL2Notes/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  TheCPL2Notes
+//
+//  Created by bytedance on 2021/11/5.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
